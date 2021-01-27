081e643e1298f44ce76b88e6dffe619e835ef060
Julian Benedikt Heuschen <julian@fheuschen.de>
2021-01-27 22:43:37 +0100

    HIBP

--- PasswordCheck/PasswordCheck.swift
+++ PasswordCheck/PasswordCheck.swift
@@ -15,17 +15,39 @@ public protocol Checker {
     
 }
 
+internal extension Checker {
+    
+    func doRequest(to: URL, then: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        let task = URLSession.shared.dataTask(with: URLRequest(url: to)) { (d, r, e) in
+            then(d, r, e)
+        }
+        task.resume()
+    }
+    
+}
+
 public struct PasswordChecker : Checker {
+    
+    private static let API = "https://api.pwnedpasswords.com/range/"
     
     private var hash: String
     
     internal init(hash: String) {
         precondition(hash.count > 5)
-        self.hash = String(hash.prefix(5))
+        self.hash = hash
     }
     
     public func execute(_ result: @escaping (Bool) -> Void) throws {
-        
+        self.doRequest(to: URL(string: Self.API + self.hash.prefix(5))!) { d, _, e in
+            if let d = d {
+                if let str = String(data: d, encoding: .utf8) {
+                    let res = str.split(whereSeparator: \.isNewline)
+                    result(!res.map { String($0.split(separator: ":")[0]).uppercased() }.contains(String(self.hash.uppercased().suffix(35))))
+                }
+            } else if let e = e {
+                NSLog(e.localizedDescription)
+            }
+        }
     }
     
 }
@@ -62,14 +84,28 @@ public class PasswordCheck {
     
 }
 
+@available(iOS 13.0, macOS 10.15, *)
+internal extension Digest {
+    var bytes: [UInt8] { Array(makeIterator()) }
+    var data: Data { Data(bytes) }
+
+    var hexStr: String {
+        bytes.map { String(format: "%02X", $0) }.joined()
+    }
+}
+
 internal extension String {
     func sha1() -> String {
-        let data = Data(self.utf8)
-        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
-        data.withUnsafeBytes {
-            _ = CC_SHA1($0.baseAddress, CC_LONG(data.count), &digest)
+        if #available(iOS 13.0, macOS 10.15, *) {
+            return Insecure.SHA1.hash(data: self.data(using: .utf8)!).hexStr
+        } else {
+            let data = Data(self.utf8)
+            var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+            data.withUnsafeBytes {
+                _ = CC_SHA1($0.baseAddress, CC_LONG(data.count), &digest)
+            }
+            let hexBytes = digest.map { String(format: "%02hhx", $0) }
+            return hexBytes.joined()
         }
-        let hexBytes = digest.map { String(format: "%02hhx", $0) }
-        return hexBytes.joined()
     }
 }
