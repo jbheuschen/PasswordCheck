4265feb5548c2a04ab1653db326d8bca55fe06d0
Julian Benedikt Heuschen <julian@fheuschen.de>
2021-01-27 20:38:01 +0100

    Initial Commit

---
+++ PasswordCheck/PasswordCheck.h
@@ -0,0 +1,18 @@
+//
+//  PasswordCheck.h
+//  PasswordCheck
+//
+//  Created by Julian Benedikt Heuschen on 1/27/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PasswordCheck.
+FOUNDATION_EXPORT double PasswordCheckVersionNumber;
+
+//! Project version string for PasswordCheck.
+FOUNDATION_EXPORT const unsigned char PasswordCheckVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PasswordCheck/PublicHeader.h>
+
+
